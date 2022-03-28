84c5d502794e0770cef766bfc562c67e032471bf
Peter Barnes <pe5erb@gmail.com>
2022-03-28 19:04:29 +0100

    preparing for initial deployment

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # inspections
 web app to display the data from inspections (such as fire door survey)
+
+# requirements
+* python 3
+* python-flask
+* flask-bootstrap
+* flask-mysqldb
--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-from flask import Flask,render_template, request
-from flask_mysqldb import MySQL
 import os
+from datetime import datetime
+
+from flask import Flask
+from flask import render_template, request, redirect
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
 # import config file with MySQL Host, User, Password and Database
 app.config.from_pyfile(os.path.join(".", "app.conf"), silent=False)
- 
 mysql = MySQL(app)
 
 # serve index page
 @app.route("/")
 def index():
-    return '''<h1>Inspections Web App - Home</h1><p>contact: webmaster@fireconsultancy.co.uk</p>'''
+    return render_template('index.html')
+
+# serve index page
+@app.route("/eb/bq/")
+def building():
+    return render_template('building.html')
 
 @app.route("/favicon.ico")
 def favicon():
@@ -21,21 +28,36 @@ def favicon():
 @app.route("/eb/bq/<asset_no>")
 def asset_url(asset_no):
 
+    try:
+        asset_no = int(asset_no)
+    except ValueError:
+        return render_template('building.html')
+
+    if(asset_no<1 or asset_no>119):
+        return render_template('building.html')
+
     #Creating a connection cursor
     cursor = mysql.connection.cursor()
     
     #Executing SQL Statements
-    query = "SELECT PhotoSticker FROM Broad_Quay WHERE DoorNumber=" + str(asset_no)
+    query = "SELECT * FROM Broad_Quay WHERE DoorNumber=" + str(asset_no)
     cursor.execute(query)
-    for results in cursor.fetchall() :
-        result = results[0]
-        print("result=",result)
-    
-    #Closing the cursor
+    for data in cursor.fetchall() :
+        print(data)
     cursor.close()
+    
+    return render_template('asset.html', data=data)
 
-    ImageURL = "<img src=\"https://inspections-assets.s3.eu-west-2.amazonaws.com/images/" + result + "\" width=\"500\">"
-    return ImageURL
+@app.route("/showAsset", methods=['POST'])
+def showAsset():
+    assetNumber = request.form['assetNumber']
+    print("going to: ",assetNumber)
+    assetURL = "/eb/bq/" + assetNumber
+    return redirect(assetURL)
+    
+@app.context_processor
+def inject_now():
+    return {'now': datetime.utcnow()}
 
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=80)
+      app.run(host='0.0.0.0', port=8081, debug=True)
